18e0a247e59f94e9cbbc8813d24f79efb12a2b53
odas0r <guilherme.odas0r@gmail.com>
2024-02-17 12:58:15 +0000

    added install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
 #!/bin/sh
+
+# Create directory for Docker cli plugins
+mkdir -p ~/.docker/cli-plugins
+
+# Download the plugin script
+curl "https://raw.githubusercontent.com/muxit-studio/docker-cleanup/main/docker-cleanup" -o ~/.docker/cli-plugins/docker-cleanup
+
+# Make the script executable
+chmod +x ~/.docker/cli-plugins/docker-cleanup
